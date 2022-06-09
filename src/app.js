5d9bb5786e6cd69dd93ff707d26d9a169ed0d7dd
Baracovama <juanvguevaram@gmail.com>
2022-06-09 16:24:53 +0000

    Proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,46 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+/*window.onload = function() {
+  let numero = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "K", "Q", "A"];
+  let palo = ["♦", "♥", "♠", "♣"];
+
+  const random = value => {
+    let index = Math.floor(Math.random() * value.length);
+    return value[index];
+  };
+
+  console.log(random(numero));
+  console.log(random(palo));
+};
+*/
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "K",
+    "Q",
+    "A"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["diamonds", "hearts", "spades", "clubs"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
 };
