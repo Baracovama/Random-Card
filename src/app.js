ecbcc53d99f26a0e3f5fe544ed3ce1e967b8deef
Baracovama <juanvguevaram@gmail.com>
2022-06-09 16:26:14 +0000

    Proyecto finalizado editado

--- src/app.js
+++ src/app.js
@@ -5,19 +5,6 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-/*window.onload = function() {
-  let numero = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "K", "Q", "A"];
-  let palo = ["♦", "♥", "♠", "♣"];
-
-  const random = value => {
-    let index = Math.floor(Math.random() * value.length);
-    return value[index];
-  };
-
-  console.log(random(numero));
-  console.log(random(palo));
-};
-*/
 window.onload = () => {
   document.querySelector(".card").classList.add(generateRandomSuit());
   document.querySelector(".card").innerHTML = generateRandomNumber();
